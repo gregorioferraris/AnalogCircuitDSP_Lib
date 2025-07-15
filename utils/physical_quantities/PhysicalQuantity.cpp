d78ae835057483cc50d3e008527183914b4bec9b
gregorioferraris <ferraris.gregorio@gmail.com>
2025-07-15 23:08:18 +0200

    Create PhysicalQuantity.cpp

---
+++ utils/physical_quantities/PhysicalQuantity.cpp
@@ -0,0 +1,5 @@
+// utils/physical_quantities/PhysicalQuantity.cpp
+#include "PhysicalQuantity.h"
+
+// Non c'è molto da implementare qui dato che la classe è quasi interamente inline o virtuale pura.
+// Le implementazioni degli operatori base sono già nel .h.
