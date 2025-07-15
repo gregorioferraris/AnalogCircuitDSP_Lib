67b630c77b6ed5516df8f0e895cfda37d8b48c26
gregorioferraris <ferraris.gregorio@gmail.com>
2025-07-15 23:36:51 +0200

    Create Subcircuit.cpp

---
+++ circuit_solver/subcircuits/Subcircuit.cpp
@@ -0,0 +1,6 @@
+// circuit_solver/subcircuits/Subcircuit.cpp
+#include "Subcircuit.h"
+#include <iostream>
+
+// Non c'è molto da implementare qui dato che la maggior parte è nel .h
+// o gestita dai metodi di Circuit.
